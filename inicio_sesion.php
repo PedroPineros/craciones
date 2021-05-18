79c765518d6e499acf79f8ab3bd8c44925da09c2
unknown <PedroPineros>
2021-05-18 18:11:22 -0500

    inciando estructura

--- inicio_sesion.php
+++ inicio_sesion.php
@@ -4,6 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio de Sesion</title>
+    <script>
+        <?php
+            require_once 'conexion/conexion.php'
+        ?>
+    </script>
 </head>
 <body>
     <label for="">1</label>
