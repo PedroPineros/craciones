58e20b3e1446e9f4015073ca2d680365b3c37444
PedroPineros <40897414+PedroPineros@users.noreply.github.com>
2021-05-18 10:46:14 -0500

    Add files via upload

---
+++ inicio_sesion.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio de Sesion</title>
+</head>
+<body>
+    <label for="">1</label>
+</body>
+</html>
