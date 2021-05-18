656eea66bfc8a4142ec6b3b14196192fa92b9ac3
unknown <PedroPineros>
2021-05-18 18:55:09 -0500

    inicio conexion php

---
+++ inicio_sesion_conexion.php
@@ -0,0 +1,23 @@
+<?php 
+/*se llama conexion base de datos conexion.php*/
+require_once 'conexion/conexion.php'   ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio de Sesion</title>
+    <script>
+        
+    </script>
+</head>
+<body>
+    <div class="container">
+        <label for="usr">Correo</label>
+        <input type="text" class="form-control" id="usr">
+        <label for="pws">Contraseña</label>
+        <input type="text" class="form-control" id="pws">
+    </div>
+</body>
+</html>
