34cd34064c12d297ec4e987ee4ba5e4e368d5b0b
unknown <PedroPineros>
2021-05-18 19:06:35 -0500

    .

---
+++ inicio_sesion/inicio_sesion.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio de Sesion</title>
+    <script>
+        
+    </script>
+</head>
+<body>
+    <div class="container">
+        <label for="usr">Correo</label>
+        <input type="text" class="form-control" id="usr">
+        <label for="pws">Contraseña</label>
+        <input type="text" class="form-control" id="pws">
+    </div>
+</body>
+</html>
